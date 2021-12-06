bbbe70501be6d26e8fd33cd9692abdc28c2c110e
pradyumna surampudi <pradyumna.surampudi@gmail.com>
2021-12-06 22:08:27 +0100

    change package.json

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  webpack5: true,
-  webpack: (config) => {
-    config.module.rules.push({
+  webpack: (configuration) => {
+    configuration.module.rules.push({
       test: /\.md$/,
       use: 'frontmatter-markdown-loader',
     })
-    return config
+    return configuration
   },
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
