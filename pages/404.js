d702011630385da7d8e1e495668a9c827d892251
Prador <pradyumna.surampudi@gmail.com>
2022-03-04 11:32:27 +0100

    added 404 error page

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+// pages/404.js
+import Layout from '../components/layout'
+import ProgramAccordion from '../components/home/ProgramAccordion'
+import { attributes, html } from '../content/home/home.md'
+
+export default function Custom404() {
+    return (
+    <Layout>
+        <div className="content home-faq-wrapper">
+      <h2 className='home-section-title'>Looks like this page was not found</h2> 
+      <h4><a href="/" class="btn btn-black">Back to our home page</a></h4>
+      {/* <h4>Learn more about any of our programs</h4> */}
+      <ProgramAccordion programs={attributes}/> 
+    </div></Layout>
+    )
+  }
+  
