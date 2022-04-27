f083cf3b10f901bd21d7e1c68c1bd0bd1be3424f
pradyumna surampudi <pradyumna.surampudi@gmail.com>
2022-04-27 08:11:28 +0200

    update doc order

--- components/content/DocumentsSection.js
+++ components/content/DocumentsSection.js
@@ -2,10 +2,12 @@ const DocBlock = ({ docs,type }) => {
 return <>
 {docs.filter((obj) => obj.attributes.certification === type).length ? <div className="doc-block">
      <h4 className="doc-block-title"><a href={"/mandatory-disclosures/category/"+type}>{type}</a></h4>
-    {docs.filter((obj) => obj.attributes.certification === type).map(({attributes}) => <>
+    {docs.filter((obj) => obj.attributes.certification === type).sort((a,b)=>(a.attributes.order>b.attributes.order ? 1 : -1)).map(({attributes}) => <>
           <div className='doc-item'>
             <a href={"/"+attributes.asset} className="doc-btn" target="_blank">
-                <p><strong>{attributes.title}:</strong> {attributes.description}</p>
+                <p>
+                  {/* <strong>{attributes.title}:</strong>  */}
+                {attributes.description}</p>
             </a>
           </div>
         </>)}
@@ -13,7 +15,7 @@ return <>
   </>
 }
 const DocumentsSection = ({ docs }) => {
-  let docTypes = ["Affiliation Copy","AISHE","AICTE","AQAR","PO&CO","Compliance with RTI","Form 10B","IACBE","IQAC","NAAC","NBA","Student Hand Book","Student feedback survey","UGC"]
+  let docTypes = ["Affiliation Copy","AISHE","AICTE","AQAR","PO&CO","Compliance with RTI","Form 10B","IACBE","IQAC","NAAC","NBA","Student feedback survey","UGC"]
   return (
   <>
     <section className="doc-wrapper">
