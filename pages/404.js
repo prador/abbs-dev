a336442294642ec42dc44bfa277cfa3806713255
pradyumna surampudi <pradyumna.surampudi@gmail.com>
2022-05-10 11:18:09 +0200

    changes to program accorion in 404

--- pages/404.js
+++ pages/404.js
@@ -4,13 +4,14 @@ import ProgramAccordion from '../components/home/ProgramAccordion'
 import { attributes, html } from '../content/home/home.md'
 
 export default function Custom404() {
+  console.log(attributes.program_accordion)
     return (
     <Layout>
         <div className="content home-faq-wrapper">
       <h2 className='home-section-title'>Looks like this page was not found</h2> 
       <h4><a href="/" class="btn btn-black">Back to our home page</a></h4>
       {/* <h4>Learn more about any of our programs</h4> */}
-      <ProgramAccordion programs={attributes}/> 
+      <ProgramAccordion programs={attributes.program_accordion}/> 
     </div></Layout>
     )
   }
