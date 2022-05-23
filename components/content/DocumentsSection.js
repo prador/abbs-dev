6845beaed1772ba3b670e64f07bd5435c6bdc170
pradyumna surampudi <pradyumna.surampudi@gmail.com>
2022-05-23 11:06:09 +0200

    added nirf data doc

--- components/content/DocumentsSection.js
+++ components/content/DocumentsSection.js
@@ -16,7 +16,7 @@ return <>
   </>
 }
 const DocumentsSection = ({ docs }) => {
-  let docTypes = ["Academics","Affiliation Copy","AISHE","AICTE","AQAR","PO&CO","Calendar of Events","Compliance with RTI","Form 10B","IACBE","IQAC","NAAC","NBA","Student feedback survey","UGC"]
+  let docTypes = ["Academics","Affiliation Copy","AISHE","AICTE","AQAR","PO&CO","Calendar of Events","Compliance with RTI","Form 10B","IACBE","IQAC","NAAC","NBA","NIRF Data","Student feedback survey","UGC"]
   return (
   <>
     <section className="doc-wrapper">
---
+++ content/docs/submitted-institute-data-for-nirf-2022.md
@@ -0,0 +1,9 @@
+---
+title: Submitted Institute Data for NIRF 2022
+date: 2022-05-23T08:54:22.489Z
+order: "1"
+description: Submitted Institute Data for NIRF 2022
+certification: NIRF Data
+asset: static/img/national-institutianal-ranking-framework.pdf
+show_toggle: true
+---
